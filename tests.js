576f8f7619e67589118b1b042744bc3e0553a6b1
Angel Fernandez <afernandez@logtrust.com>
2016-03-17 02:11:12 +0100

    Refactor

--- tests.js
+++ tests.js
@@ -10,7 +10,14 @@ define(['lodash'], function (_) {
    ****** FIXED DATA ********
    *************************/
 
-  testFunctions.fixedData.testMapReduce = function (d) {
+  /**
+   * First. Use map to convert String date to Epoch.
+   * Then, sort by epoch.
+   * Finally, reduce to concatenate the values for repeated epochs.
+   * The reduce function is made using an Array.
+   * @param d
+   */
+  testFunctions.fixedData.arrayMapSortReduceWithArray = function (d) {
 
     d.map(function (el) {
       el.k[3] = new Date(el.k[3]).getTime();
@@ -19,15 +26,19 @@ define(['lodash'], function (_) {
       return a.k[3] - b.k[3]
     }).reduce(function (total, current) {
       var totalLen = total.length;
-      var currentIndex = totalLen - 1;
-      if (totalLen && total[currentIndex].period == current.k[3]) {
-        total[currentIndex].values[0].push(current.k[1]);
-        total[currentIndex].values[1].push(current.k[2]);
-        total[currentIndex].values[2].push(current.v[0]);
+      var lastIndex = totalLen - 1;
+      var key = current.k[3];
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      if (totalLen && total[lastIndex].period === key) {
+        total[lastIndex].values[0].push(v0);
+        total[lastIndex].values[1].push(v1);
+        total[lastIndex].values[2].push(v2);
       } else {
         total.push({
-          period: current.k[3],
-          values: [[current.k[1]], [current.k[2]], [current.v[0]]]
+          period: key,
+          values: [[v0], [v1], [v2]]
         });
       }
       return total;
@@ -35,22 +46,33 @@ define(['lodash'], function (_) {
 
   };
 
-  testFunctions.fixedData.testReduceWithObject = function (d) {
+  /**
+   * First. Use map to convert String date to Epoch.
+   * Then, sort by epoch.
+   * Finally, reduce to concatenate the values for repeated epochs.
+   * The reduce function is made using an Object.
+   * @param d
+   */
+  testFunctions.fixedData.arrayMapSortReduceWithObject = function (d) {
 
-    d.reduce(function (total, current) {
-      var millis = new Date(current.k[3]).getTime();
-      var v0 = current.k[0];
-      var v1 = current.k[1];
-      var v2 = current.k[2];
-
-      if (millis in total) {
-        total[millis].values[0].push(v1);
-        total[millis].values[1].push(v2);
-        total[millis].values[2].push(v0);
+    d.map(function (el) {
+      el.k[3] = new Date(el.k[3]).getTime();
+      return el;
+    }).sort(function (a, b) {
+      return a.k[3] - b.k[3]
+    }).reduce(function (total, current) {
+      var key = current.k[3];
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      if (key in total) {
+        total[key].values[0].push(v0);
+        total[key].values[1].push(v1);
+        total[key].values[2].push(v2);
       } else {
-        total[millis] = {
-          period: millis,
-          values: [[v1], [v2], [v0]]
+        total[key] = {
+          period: key,
+          values: [[v0], [v1], [v2]]
         };
       }
       return total;
@@ -59,89 +81,135 @@ define(['lodash'], function (_) {
   };
 
   /**
-   * Realmente el map no es necesario dado que el valor de la fecha a timestamp
-   * se puede aplicar directamente en el reduce y me follo el map
+   * Use of reduce to concatenate the values for repeated epochs.
+   * Meanwhile, result is sorted by key.
+   * The result is an Array
+   * @param d
    */
-  testFunctions.fixedData.testReduceWithArray = function (d) {
+  testFunctions.fixedData.arrayReduceWithArray = function (d) {
 
     d.reduce(function (total, current) {
-      var millis = new Date(current.k[3]).getTime();
+      var key = new Date(current.k[3]).getTime();
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
       var json;
-      for (var j = 0; j <= total.length; j++) {
-        if (j == total.length || total[j].period > millis) {
+      var len = total.length;
+      for (var j = 0; j <= len; j++) {
+        if (j == len || total[j].period > key) {
           json = {
-            period: millis,
-            values: [[], [], []]
+            period: key,
+            values: [[v0], [v1], [v2]]
           };
           total.splice(j, 0, json);
         }
-        if (total[j].period == millis) {
-          json = total[j];
+        if (total[j].period == key) {
+          total[j].values[0].push(v0);
+          total[j].values[1].push(v1);
+          total[j].values[2].push(v2);
           break;
         }
       }
-      json.values[0].push(current.k[1]);
-      json.values[1].push(current.k[2]);
-      json.values[2].push(current.v[0]);
       return total;
     }, []);
 
   };
 
   /**
-   * Version iterativa
+   * Use of reduce to concatenate the values for repeated epochs.
+   * The result is an Object
+   * @param d
    */
-  testFunctions.fixedData.testIterative = function (d) {
+  testFunctions.fixedData.arrayReduceWithObject = function (d) {
+
+    d.reduce(function (total, current) {
+      var key = new Date(current.k[3]).getTime();
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+
+      if (key in total) {
+        total[key].values[0].push(v0);
+        total[key].values[1].push(v1);
+        total[key].values[2].push(v2);
+      } else {
+        total[key] = {
+          period: key,
+          values: [[v0], [v1], [v2]]
+        };
+      }
+      return total;
+    }, {});
+
+  };
+
+  /**
+   * Iterate over data meanwhile the result is sorted by key.
+   * Concatenates values with same key.
+   * @param d
+   */
+  testFunctions.fixedData.iterative = function (d) {
 
     var parsed_data = [];
-    for (var i = 0; i < d.length; i++) {
-      var millis = new Date(d[i].k[3]).getTime();
+    var tot = d.length;
+    for (var i = 0; i < tot; i++) {
+      var key = new Date(d[i].k[3]).getTime();
+      var v0 = d[i].k[1];
+      var v1 = d[i].k[2];
+      var v2 = d[i].v[0];
       var json;
-      for (var j = 0; j <= parsed_data.length; j++) {
-        if (j == parsed_data.length || parsed_data[j].period > millis) {
+      var len = parsed_data.length;
+      for (var j = 0; j <= len; j++) {
+        if (j == len || parsed_data[j].period > key) {
           json = {
-            period: millis,
-            values: [[], [], []]
+            period: key,
+            values: [[v0], [v1], [v2]]
           };
           parsed_data.splice(j, 0, json);
         }
-        if (parsed_data[j].period == millis) {
-          json = parsed_data[j];
+        if (parsed_data[j].period == key) {
+          parsed_data[j].values[0].push(v0);
+          parsed_data[j].values[1].push(v1);
+          parsed_data[j].values[2].push(v2);
           break;
         }
       }
-      json.values[0].push(d[i].k[1]);
-      json.values[1].push(d[i].k[2]);
-      json.values[2].push(d[i].v[0]);
     }
 
   };
 
-  testFunctions.fixedData.testIterativeSort = function (d) {
+  /**
+   * Iterate over data previously sorted by key.
+   * Concatenates values with same key.
+   * @param d
+   */
+  testFunctions.fixedData.iterativeAndSort = function (d) {
 
     var total = [];
     d = d.sort(function (a, b) {
-        if ("string" === typeof a.k[3]) {
-          a.k[3] = new Date(a.k[3]).getTime()
-        }
-        if ("string" === typeof b.k[3]) {
-          b.k[3] = new Date(b.k[3]).getTime()
-        }
-        return a.k[3] - b.k[3]
+      if ("string" === typeof a.k[3]) {
+        a.k[3] = new Date(a.k[3]).getTime();
       }
-    );
+      if ("string" === typeof b.k[3]) {
+        b.k[3] = new Date(b.k[3]).getTime();
+      }
+      return a.k[3] - b.k[3];
+    });
     var currentIndex = -1;
-    for (var i = 0; i < d.length; i++) {
-      var millis = d[i].k[3];
-      if (currentIndex >= 0 &&
-        total[currentIndex].period == millis) {
-        total[currentIndex].values[0].push(d[i].k[1]);
-        total[currentIndex].values[1].push(d[i].k[2]);
-        total[currentIndex].values[2].push(d[i].v[0]);
+    var len = d.length;
+    for (var i = 0; i < len; i++) {
+      var key = d[i].k[3];
+      var v0 = d[i].k[1];
+      var v1 = d[i].k[2];
+      var v2 = d[i].v[0];
+      if (currentIndex >= 0 && total[currentIndex].period == key) {
+        total[currentIndex].values[0].push(v0);
+        total[currentIndex].values[1].push(v1);
+        total[currentIndex].values[2].push(v2);
       } else {
         total.push({
-          period: millis,
-          values: [[d[i].k[1]], [d[i].k[2]], [d[i].v[0]]]
+          period: key,
+          values: [[v0], [v1], [v2]]
         });
         ++currentIndex;
       }
@@ -149,8 +217,15 @@ define(['lodash'], function (_) {
 
   };
 
-  //LODASH MAP REDUCE
-  testFunctions.fixedData.lodashChainMapReduceWithArray = function (d) {
+  /**
+   * Using lodash with chain.
+   * First. Use map to convert String date to Epoch.
+   * Then, sort by epoch.
+   * Finally, reduce to concatenate the values for repeated epochs.
+   * The reduce function is made using an Array.
+   * @param d
+   */
+  testFunctions.fixedData.lodashChainMapSortReduceWithArray = function (d) {
 
     _.chain(d).map(function (el) {
       el.k[3] = new Date(el.k[3]).getTime();
@@ -159,14 +234,18 @@ define(['lodash'], function (_) {
       return a.k[3] - b.k[3]
     }).reduce(function (total, current) {
       var currentIndex = total.length - 1;
-      if (total.length && total[currentIndex].period == current.k[3]) {
-        total[currentIndex].values[0].push(current.k[1]);
-        total[currentIndex].values[1].push(current.k[2]);
-        total[currentIndex].values[2].push(current.v[0]);
+      var key = current.k[3];
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      if (total.length && total[currentIndex].period == key) {
+        total[currentIndex].values[0].push(v0);
+        total[currentIndex].values[1].push(v1);
+        total[currentIndex].values[2].push(v2);
       } else {
         total.push({
-          period: current.k[3],
-          values: [[current.k[1]], [current.k[2]], [current.v[0]]]
+          period: key,
+          values: [[v0], [v1], [v2]]
         });
       }
       return total;
@@ -174,29 +253,120 @@ define(['lodash'], function (_) {
 
   };
 
-  testFunctions.fixedData.lodashReduceWithObject = function (d) {
+  /**
+   * Using lodash with chain.
+   * First. Use map to convert String date to Epoch.
+   * Then, sort by epoch.
+   * Finally, reduce to concatenate the values for repeated epochs.
+   * The reduce function is made using an Object.
+   * @param d
+   */
+  testFunctions.fixedData.lodashChainMapSortReduceWithObject = function (d) {
+
+    _.chain(d).map(function (el) {
+      el.k[3] = new Date(el.k[3]).getTime();
+      return el;
+    }).sort(function (a, b) {
+      return a.k[3] - b.k[3]
+    }).reduce(function (total, current) {
+      var key = current.k[3];
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      if (key in total) {
+        total[key].values[0].push(v0);
+        total[key].values[1].push(v1);
+        total[key].values[2].push(v2);
+      } else {
+        total[key] = {
+          period: key,
+          values: [[v0], [v1], [v2]]
+        };
+      }
+      return total;
+    }, {}).value();
+
+  };
+
+  /**
+   * Using lodash with chain
+   * Use of reduce to concatenate the values for repeated epochs.
+   * Meanwhile, result is sorted by key.
+   * The result is an Array
+   * @param d
+   */
+  testFunctions.fixedData.lodashChainReduceWithArray = function (d) {
+
+    _.chain(d).reduce(function (total, current) {
+      var key = new Date(current.k[3]).getTime();
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      var json;
+      var len = total.length;
+      for (var j = 0; j <= len; j++) {
+        if (j == len || total[j].period > key) {
+          json = {
+            period: key,
+            values: [[v0], [v1], [v2]]
+          };
+          total.splice(j, 0, json);
+        }
+        if (total[j].period == key) {
+          total[j].values[0].push(v0);
+          total[j].values[1].push(v1);
+          total[j].values[2].push(v2);
+          break;
+        }
+      }
+      return total;
+    }, {}).value();
+
+  };
+
+  /**
+   * Using lodash without chain
+   * Use of reduce to concatenate the values for repeated epochs.
+   * Meanwhile, result is sorted by key.
+   * The result is an Array
+   * @param d
+   */
+  testFunctions.fixedData.lodashReduceWithArray = function (d) {
 
     _.reduce(d, function (total, current) {
-      var millis = new Date(current.k[3]).getTime();
-      var v0 = current.k[0];
-      var v1 = current.k[1];
-      var v2 = current.k[2];
-
-      if (millis in total) {
-        total[millis].values[0].push(v1);
-        total[millis].values[1].push(v2);
-        total[millis].values[2].push(v0);
-      } else {
-        total[millis] = {
-          period: millis,
-          values: [[v1], [v2], [v0]]
-        };
+      var key = new Date(current.k[3]).getTime();
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      var json;
+      var len = total.length;
+      for (var j = 0; j <= len; j++) {
+        if (j == len || total[j].period > key) {
+          json = {
+            period: key,
+            values: [[v0], [v1], [v2]]
+          };
+          total.splice(j, 0, json);
+        }
+        if (total[j].period == key) {
+          total[j].values[0].push(v0);
+          total[j].values[1].push(v1);
+          total[j].values[2].push(v2);
+          break;
+        }
       }
       return total;
     }, {});
 
   };
 
+  /**
+   * Using lodash with chain
+   * Use of reduce to concatenate the values for repeated epochs.
+   * Meanwhile, result is sorted by key.
+   * The result is an Object
+   * @param d
+   */
   testFunctions.fixedData.lodashChainReduceWithObject = function (d) {
 
     _.chain(d).reduce(function (total, current) {
@@ -220,8 +390,44 @@ define(['lodash'], function (_) {
 
   };
 
-  //LODASH MAP TRANSFORM
-  testFunctions.fixedData.lodashChainMapTransform = function (d) {
+  /**
+   * Using lodash without chain
+   * Use of reduce to concatenate the values for repeated epochs.
+   * Meanwhile, result is sorted by key.
+   * The result is an Object
+   * @param d
+   */
+  testFunctions.fixedData.lodashReduceWithObject = function (d) {
+
+    _.reduce(d, function (total, current) {
+      var millis = new Date(current.k[3]).getTime();
+      var v0 = current.k[0];
+      var v1 = current.k[1];
+      var v2 = current.k[2];
+
+      if (millis in total) {
+        total[millis].values[0].push(v1);
+        total[millis].values[1].push(v2);
+        total[millis].values[2].push(v0);
+      } else {
+        total[millis] = {
+          period: millis,
+          values: [[v1], [v2], [v0]]
+        };
+      }
+      return total;
+    }, {});
+
+  };
+
+  /**
+   * First. Use map to convert String date to Epoch.
+   * Then, sort by epoch.
+   * Finally, reduce to concatenate the values for repeated epochs.
+   * The transform function is made using an Array.
+   * @param d
+   */
+  testFunctions.fixedData.lodashChainMapSortTransformWithArray = function (d) {
 
     _.chain(d).map(function (el) {
       el.k[3] = new Date(el.k[3]).getTime();
@@ -229,19 +435,58 @@ define(['lodash'], function (_) {
     }).sort(function (a, b) {
       return a.k[3] - b.k[3]
     }).transform(function (total, current) {
-      var currentIndex = total.length - 1;
-      if (total.length && total[currentIndex].period == current.k[3]) {
-        total[currentIndex].values[0].push(current.k[1]);
-        total[currentIndex].values[1].push(current.k[2]);
-        total[currentIndex].values[2].push(current.v[0]);
+      var totalLen = total.length;
+      var lastIndex = totalLen - 1;
+      var key = current.k[3];
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      if (totalLen && total[lastIndex].period === key) {
+        total[lastIndex].values[0].push(v0);
+        total[lastIndex].values[1].push(v1);
+        total[lastIndex].values[2].push(v2);
       } else {
         total.push({
-          period: current.k[3],
-          values: [[current.k[1]], [current.k[2]], [current.v[0]]]
+          period: key,
+          values: [[v0], [v1], [v2]]
         });
       }
       return total;
     }, []).value();
+
+  };
+
+  /**
+   * First. Use map to convert String date to Epoch.
+   * Then, sort by epoch.
+   * Finally, reduce to concatenate the values for repeated epochs.
+   * The transform function is made using an Object.
+   * @param d
+   */
+  testFunctions.fixedData.lodashChainMapSortTransformWithObject = function (d) {
+
+    _.chain(d).map(function (el) {
+      el.k[3] = new Date(el.k[3]).getTime();
+      return el;
+    }).sort(function (a, b) {
+      return a.k[3] - b.k[3]
+    }).transform(function (total, current) {
+      var key = current.k[3];
+      var v0 = current.k[1];
+      var v1 = current.k[2];
+      var v2 = current.v[0];
+      if (key in total) {
+        total[key].values[0].push(v0);
+        total[key].values[1].push(v1);
+        total[key].values[2].push(v2);
+      } else {
+        total[key] = {
+          period: key,
+          values: [[v0], [v1], [v2]]
+        };
+      }
+      return total;
+    }, {}).value();
 
   };
 
@@ -250,8 +495,11 @@ define(['lodash'], function (_) {
    ****** RANDOM DATA *******
    *************************/
 
-    // NATIVE MAP REDUCE
-  testFunctions.randomData.testMapReduce1 = function (d) {
+  /**
+   * Native map reduce
+   * @param d
+   */
+  testFunctions.randomData.arrayMapReduce = function (d) {
 
     d.map(function (el) {
       return el % 2 === 0 ? el / 2 : 0;
@@ -261,8 +509,11 @@ define(['lodash'], function (_) {
 
   };
 
-  // NATIVE REDUCE
-  testFunctions.randomData.testOnlyReduce = function (d) {
+  /**
+   * Native reduce
+   * @param d
+   */
+  testFunctions.randomData.arrayReduce = function (d) {
 
     d.reduce(function (total, current) {
       return total + (current % 2 === 0 ? current / 2 : 0);
@@ -270,8 +521,11 @@ define(['lodash'], function (_) {
 
   };
 
-  // NATIVE WHILE
-  testFunctions.randomData.testIterative = function (d) {
+  /**
+   * Native while
+   * @param d
+   */
+  testFunctions.randomData.iterative = function (d) {
 
     var total = 0;
     var index = d.length;
@@ -281,8 +535,11 @@ define(['lodash'], function (_) {
 
   };
 
-  // LODASH MAP REDUCE
-  testFunctions.randomData.lodashMR = function (d) {
+  /**
+   * Lodash Map reduce
+   * @param d
+   */
+  testFunctions.randomData.lodashMapReduce = function (d) {
 
     _.chain(d).map(function (el) {
       return el % 2 === 0 ? el / 2 : 0;
@@ -292,8 +549,11 @@ define(['lodash'], function (_) {
 
   };
 
-  // LODASH MAP TRANSFORM
-  testFunctions.randomData.lodashMT = function (d) {
+  /**
+   * Lodash Map Transform
+   * @param d
+   */
+  testFunctions.randomData.lodashMapTransform = function (d) {
 
     _.chain(d).map(function (el) {
       return el % 2 === 0 ? el / 2 : 0;
@@ -303,7 +563,10 @@ define(['lodash'], function (_) {
 
   };
 
-  // LODASH REDUCE
+  /**
+   * Lodash Reduce
+   * @param d
+   */
   testFunctions.randomData.lodashReduce = function (d) {
 
     _.reduce(d, function (total, current) {
@@ -312,7 +575,10 @@ define(['lodash'], function (_) {
 
   };
 
-  // LODASH TRANSFORM
+  /**
+   * Lodash Transform
+   * @param d
+   */
   testFunctions.randomData.lodashTransform = function (d) {
 
     _.transform(d, function (total, current) {
@@ -320,7 +586,6 @@ define(['lodash'], function (_) {
     }, 0);
 
   };
-
 
   return {
     'testFunctions': testFunctions
